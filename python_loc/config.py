f703dbfc04944c2cf1c9047a3e4025de40f4640f
Roman Penyaev <r.peniaev@gmail.com>
2021-07-26 22:57:50 +0200

    python_loc/config: update landing pad dimensions

--- python_loc/config.py
+++ python_loc/config.py
@@ -18,11 +18,11 @@ UDP_PLOT_IP = '127.0.0.1'
 UDP_PLOT_PORT = 5558
 
 # Anchors coords
-A = np.array([0.54,0.54,0.00]) # 2585
-B = np.array([0.54,0.00,0.00]) # 262d
-C = np.array([0.00,0.54,0.00]) # 260f
-D = np.array([0.00,0.00,0.00]) # 2852
+A = np.array([0.60,0.56,0.00]) # 2585
+B = np.array([0.56,0.00,0.00]) # 262d
+C = np.array([0.04,0.60,0.00]) # 260f
+D = np.array([0.00,0.04,0.00]) # 28b9
 
 # Landing point in meters, middle of the landing platform
-LANDING_X = 0.54 / 2
-LANDING_Y = 0.54 / 2
+LANDING_X = 0.60 / 2
+LANDING_Y = 0.60 / 2
