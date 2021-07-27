7f46cfb645f168e214fba849e082e49fdb9a2c14
Roman Penyaev <r.peniaev@gmail.com>
2021-07-27 21:38:03 +0200

    python_loc/config: make landing pad dimension less accurate

--- python_loc/config.py
+++ python_loc/config.py
@@ -18,10 +18,10 @@ UDP_PLOT_IP = '127.0.0.1'
 UDP_PLOT_PORT = 5558
 
 # Anchors coords
-A = np.array([0.60,0.56,0.00]) # 2585
-B = np.array([0.56,0.00,0.00]) # 262d
-C = np.array([0.04,0.60,0.00]) # 260f
-D = np.array([0.00,0.04,0.00]) # 28b9
+A = np.array([0.60,0.60,0.00]) # 2585
+B = np.array([0.60,0.00,0.00]) # 262d
+C = np.array([0.00,0.60,0.00]) # 260f
+D = np.array([0.00,0.00,0.00]) # 28b9
 
 # Landing point in meters, middle of the landing platform
 LANDING_X = 0.60 / 2
