c4582a938b5e8aab9e370f3a5bad8b3ac8b416ea
Roman Penyaev <r.peniaev@gmail.com>
2022-04-04 17:43:24 +0200

    python_loc/file_to_sock: add tool arguments like --file and --trajectory

--- python_loc/file_to_sock.py
+++ python_loc/file_to_sock.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python3
 
+"""File to socket
+
+Usage:
+  file_to_sock.py --file <file> [--trajectory]
+
+Options:
+  -h --help          Show this screen
+  --file <file>      File with logged data, e.g. data/field-session-2/log.6.leastsq.manual
+  --trajectory       File data is a true trajectory with x,y,z
+"""
+
+from docopt import docopt
 import struct
 import time
 import socket
 import ctypes
 
-file1 = open('data/field-session-2/log.6.leastsq.manual', 'r')
-
 MCAST_GRP = '224.1.1.1'
 MCAST_PORT = 5555
 MULTICAST_TTL = 2
 
-
 PARROT_IP = "127.0.0.1"
 PARROT_PORT = 5556
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
-
-parot_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 last_ts = 0
 last_ts_s = 0
@@ -39,107 +43,115 @@ def send_parrot_data(line):
     struct.pack_into(fmt, buff, 0, last_ts_s, last_ts_us, alt, roll, pitch, yaw)
     parot_sock.sendto(buff, (PARROT_IP, PARROT_PORT))
 
+if __name__ == '__main__':
+    args = docopt(__doc__)
 
-while True:
-    line = file1.readline()
-    if not line:
-        break
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
+    parot_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    data_file = open(args['--file'], 'r')
 
-    if line.startswith("## get parrot data:"):
-        send_parrot_data(line)
-        continue
-
-    if not line.startswith("ts:"):
-        continue
-    line = line.replace(" ", "")
-
-    ts = line[line.find(":")+1 : line.find("[")]
-    ts_s , ts_us = ts.split(".")
-    ts_s = int(ts_s)
-    ts_us = int(ts_us)
-
-    vals = line[line.find("[")+1 : line.find("]")]
-    vals = vals.split(",")
-    pos_x = int(vals[0])
-    pos_y = int(vals[1])
-    pos_z = int(vals[2])
-    pos_qf = int(vals[3])
-    pos_valid = 1 # is it?
-
-    anchor_cnt = 0
-    start = 0
-    anchors = []
-
-    while (anchor_cnt < 4):
-        start = line.find("a:", start)
-        if start == -1:
+    while True:
+        line = data_file.readline()
+        if not line:
             break
 
-        addr = line[start+2 : line.find("[", start)]
-        addr = int(addr, base=16)
+        if line.startswith("## get parrot data:"):
+            send_parrot_data(line)
+            continue
 
-        anchor_pos = line[line.find("[", start)+1 : line.find("]", start)]
-        x, y, z, pos_qf = anchor_pos.split(",")
-        pos_qf = int(pos_qf)
+        if not line.startswith("ts:"):
+            continue
+        line = line.replace(" ", "")
 
-        dist = line[line.find("d=", start)+2 : line.find(",qf=", start)]
-        dist = int(dist)
+        ts = line[line.find(":")+1 : line.find("[")]
+        ts_s , ts_us = ts.split(".")
+        ts_s = int(ts_s)
+        ts_us = int(ts_us)
 
-        dist_qf = line[line.find("qf=", start)+3 : line.find("#", start)]
-        dist_qf = int(dist_qf)
+        vals = line[line.find("[")+1 : line.find("]")]
+        vals = vals.split(",")
+        pos_x = int(vals[0])
+        pos_y = int(vals[1])
+        pos_z = int(vals[2])
+        pos_qf = int(vals[3])
+        pos_valid = 1 # is it?
 
-        #
-        # Be aware that we have all saved coords and distances in mm
-        #
+        anchor_cnt = 0
+        start = 0
+        anchors = []
 
-        anchor = {
-            'addr': addr,
-            'pos': {
-                'coords':  [int(x), int(y), int(z)],
-                'qf': pos_qf,
-                'valid': pos_valid,
-            },
-            'dist': {
-                'dist': int(dist),
-                'qf': dist_qf
-            },
-        }
+        while (anchor_cnt < 4):
+            start = line.find("a:", start)
+            if start == -1:
+                break
 
-        anchors.append(anchor)
-        start += 1
-        anchor_cnt += 1
+            addr = line[start+2 : line.find("[", start)]
+            addr = int(addr, base=16)
 
-    nr_anchors = len(anchors)  # is is not used
+            anchor_pos = line[line.find("[", start)+1 : line.find("]", start)]
+            x, y, z, pos_qf = anchor_pos.split(",")
+            pos_qf = int(pos_qf)
 
-    fmt = "iiihhiii"
-    buff = ctypes.create_string_buffer(512)
+            dist = line[line.find("d=", start)+2 : line.find(",qf=", start)]
+            dist = int(dist)
 
-    # (x, y, z, pos_qf, pos_valid, ts_sec, ts_usec, nr_anchors)
-    struct.pack_into(fmt, buff, 0, pos_x, pos_y, pos_z, pos_qf, pos_valid, ts_s, ts_us, nr_anchors)
-    off = struct.calcsize(fmt)
+            dist_qf = line[line.find("qf=", start)+3 : line.find("#", start)]
+            dist_qf = int(dist_qf)
 
-    for anchor in anchors:
-        fmt = "iiihhihh"
+            #
+            # Be aware that we have all saved coords and distances in mm
+            #
 
-        coords = anchor["pos"]["coords"]
+            anchor = {
+                'addr': addr,
+                'pos': {
+                    'coords':  [int(x), int(y), int(z)],
+                    'qf': pos_qf,
+                    'valid': pos_valid,
+                },
+                'dist': {
+                    'dist': int(dist),
+                    'qf': dist_qf
+                },
+            }
 
-        # (x, y, z, pos_qf, pos_valid, dist, addr, dist_qf)
-        struct.pack_into(fmt, buff, off,
-                         coords[0], coords[1], coords[2],
-                         anchor["pos"]["qf"], anchor["pos"]["valid"],
-                         anchor["dist"]["dist"], anchor["addr"],
-                         anchor["dist"]["qf"])
-        off += struct.calcsize(fmt)
+            anchors.append(anchor)
+            start += 1
+            anchor_cnt += 1
 
-    if last_ts != 0:
-        delay = (float(ts) - last_ts) / speed
-        print("cur time: {} delay: {}".format(float(ts), delay))
-        time.sleep(delay)
-        last_ts = float(ts)
-        last_ts_s = ts_s
-        last_ts_us = ts_us
+        nr_anchors = len(anchors)  # is is not used
 
-    else:
-        last_ts = float(ts)
+        fmt = "iiihhiii"
+        buff = ctypes.create_string_buffer(512)
 
-    sock.sendto(buff, (MCAST_GRP, MCAST_PORT))
+        # (x, y, z, pos_qf, pos_valid, ts_sec, ts_usec, nr_anchors)
+        struct.pack_into(fmt, buff, 0,
+                         pos_x, pos_y, pos_z, pos_qf, pos_valid,
+                         ts_s, ts_us, nr_anchors)
+        off = struct.calcsize(fmt)
+
+        for anchor in anchors:
+            fmt = "iiihhihh"
+            coords = anchor["pos"]["coords"]
+
+            # (x, y, z, pos_qf, pos_valid, dist, addr, dist_qf)
+            struct.pack_into(fmt, buff, off,
+                             coords[0], coords[1], coords[2],
+                             anchor["pos"]["qf"], anchor["pos"]["valid"],
+                             anchor["dist"]["dist"], anchor["addr"],
+                             anchor["dist"]["qf"])
+            off += struct.calcsize(fmt)
+
+        if last_ts != 0:
+            delay = (float(ts) - last_ts) / speed
+            print("cur time: {} delay: {}".format(float(ts), delay))
+            time.sleep(delay)
+            last_ts = float(ts)
+            last_ts_s = ts_s
+            last_ts_us = ts_us
+
+        else:
+            last_ts = float(ts)
+
+        sock.sendto(buff, (MCAST_GRP, MCAST_PORT))
